ce62e1ec904bbf9605e3784d23ca6b673a197ab2
Steven Rawlinson <srawlin@gmail.com>
2014-09-11 23:17:30 -0400

    fixed database setting

--- zingexchange/settings.py
+++ zingexchange/settings.py
@@ -91,7 +91,7 @@ AUTHENTICATION_BACKENDS = ('django.contrib.auth.backends.ModelBackend',)
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+DATABASES['default'] =  dj_database_url.config(default='postgres://localhost')
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
